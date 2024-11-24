0d96fc01009639830f8294faeb73cff73bcf66f8
= <gabriel.cirilo.rosa@gmail.com>
2024-11-24 13:09:49 -0300

    Estruturacao de Page de historico

---
+++ src/app/core/interfaces/historic.ts
@@ -0,0 +1,6 @@
+export interface historic {
+  title: string;
+  message: string;
+  date: string;
+  viewed: boolean;
+}
--- src/app/features/analysis/history/history.component.ts
+++ src/app/features/analysis/history/history.component.ts
@@ -1,12 +1,28 @@
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { historic } from './../../../core/interfaces/historic';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [MatButtonToggleModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
 export class HistoryComponent {
-
+  historic: historic[] = [
+    { 
+      title: 'Novo comentário', 
+      message: 'Você recebeu um novo comentário em seu post.', 
+      date: '2024-11-24 10:30', 
+      viewed: false 
+    },
+    { 
+      title: 'Atualização do sistema', 
+      message: 'Uma nova versão do sistema está disponível.', 
+      date: '2024-11-23 14:15', 
+      viewed: true,
+    },
+  ]
+  ngOnInit(): void {}
 }
